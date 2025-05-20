d8d19859c87db481890a2f785cd1358d67bdbe93
Dirza1 <jasper.olthof@xs4all.nl>
2025-05-20 22:08:35 +0200

    import the training file and make a default dict with the training per operator

--- src/training/training.py
+++ src/training/training.py
@@ -1,9 +1,24 @@
 import csv
+from collections import defaultdict
 
 def main() -> None:
-    print("hello world")
+    training_status: defaultdict = defaultdict(list)
+    with open("report (46).csv","r",encoding="utf-8",errors="ignore") as file:
+        reader = csv.DictReader(file,delimiter=",")
+        for row in reader:
+            name = row["First Name"] + row["Last Name"]
+            
+            if any(row["Curriculum Title"] == t for t, _ in training_status[name]):
+                continue
 
-
+            if row["Curriculum Complete"] == "Yes":
+                training_status[name].append((row["Curriculum Title"],True))
+            else:
+                training_status[name].append((row["Curriculum Title"],False))
+    
+    for name,training in training_status.items():
+        for check in training:
+            print(f"{name} is {check[1]} voor {check[0]}")
 
 
 
