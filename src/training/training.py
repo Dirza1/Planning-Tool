4d9c90460e5954f74abfbc233a04e0a250e7495d
Dirza1 <jasper.olthof@xs4all.nl>
2025-05-20 22:11:00 +0200

    added small docstring to method

--- src/training/training.py
+++ src/training/training.py
@@ -2,6 +2,11 @@ import csv
 from collections import defaultdict
 
 def main() -> None:
+    """
+    This function is to create a default dict. This dict contains the operator name as a key and then a list of tupols with training status.
+    This tupol is (Curiculum name, Bool). This dictionary will be passed on to the function that will display if people are trained.
+    """
+    
     training_status: defaultdict = defaultdict(list)
     with open("report (46).csv","r",encoding="utf-8",errors="ignore") as file:
         reader = csv.DictReader(file,delimiter=",")
@@ -19,11 +24,6 @@ def main() -> None:
     for name,training in training_status.items():
         for check in training:
             print(f"{name} is {check[1]} voor {check[0]}")
-
-
-
-
-
 
 if __name__ == "__main__":
     main()
