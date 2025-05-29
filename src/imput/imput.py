552d0ae73b26da1e0fac34d3da3c17872fe49e6d
jasper.olthof <jasper.olthof@thermofisher.com>
2025-05-29 10:51:59 +0200

    added basic strutur for daily tasks

--- src/imput/imput.py
+++ src/imput/imput.py
@@ -1,10 +1,26 @@
 import csv
-
+from collections import defaultdict
 
 def main():
 
+    daily_tasks: defaultdict = defaultdict(list)
     with open("imput.csv", encoding="UTF-8", errors="ignore") as file:
-        pass
+        raw_reader = csv.reader(file)
+        raw_headers = next(raw_reader)
+        clean_headers = [h.strip().lstrip('\ufeff') for h in raw_headers]
+        reader = csv.DictReader(file, fieldnames=clean_headers, delimiter=",")
+        
+        for row in reader:
+            task_name = row["task"]
+            task_duration = row["duration (in hours)"]
+            task_title = row["title"]
+            task_related_quriculum = row["related quriculum"]
+
+            total_task = (task_duration, task_title, task_related_quriculum)
+            daily_tasks[task_name] = total_task
+        
+    
+    print(daily_tasks)
 
 
 
